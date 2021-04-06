38aa49527919d86028747d8b98f42aa34cae01f5
MrRize-web <54984438+MrRize-web@users.noreply.github.com>
2021-04-06 12:17:15 +0200

    AAKKA

---
+++ src/fr/declaration/variable.java
@@ -0,0 +1,9 @@
+package fr.declaration;
+
+import java.util.Arrays;
+
+public class variable {
+    public static void main( String[] args){
+        Arrays.asList( args ).forEach( System.out::println );
+    }
+}
